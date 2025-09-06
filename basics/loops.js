d742b10991d3c8aef3d6f8f52376cdae3aff82c9
Riechel <kriechel@amazon.de>
2025-09-06 16:09:23 +0200

    Solved Labs

--- basics/loops.js
+++ basics/loops.js
@@ -1,1 +1,33 @@
 console.log("Loops");
+
+// for loop
+//Interator is inside the for loop
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+const nameArray = ["Albert", "Bernard", "charlie", "Daniel"];
+
+for (let i = 0; i < nameArray.length; i++) {
+  console.log(nameArray[i]);
+}
+
+// while loop
+//Interator is outside the for loop
+
+let counter = 1;
+
+while (counter <= 10) {
+  console.log(counter);
+  //modiciation must be inside the while loop
+  counter++;
+}
+
+// for of loop
+//To interate through an array
+
+for (const userName of nameArray) {
+  console.log(userName);
+}
+ 
+console.log("This code has reached this line");
